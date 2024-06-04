ad8a757ed96c53c754258c8add0a3030a997453c
Mr-MT1234 <64606769+Mr-MT1234@users.noreply.github.com>
2024-06-04 16:42:56 +0200

    Goal 1

--- ei/main_view.py
+++ ei/main_view.py
@@ -2,17 +2,56 @@ import cv2
 import numpy as np
 import pygame.image
 import io
-from PIL import Image
 import cmath
+
+from dataclasses import dataclass
 
 from gfs.gui.interface import Interface
 from gfs.gui.used import Used
 from gfs.pallet import IVORY, DARKBLUE
+from gfs.fonts import MOTO_MANGUCODE_50, render_font
+
+from pycdr2 import IdlStruct
+from pycdr2.types import uint32, float32
+from typing import List
+
+
+@dataclass
+class Time(IdlStruct, typename="Time"):
+    sec: uint32
+    nsec: uint32
+
+
+@dataclass
+class Header(IdlStruct, typename="Header"):
+    stamp: Time
+    frame_id: str
+
+
+@dataclass
+class LaserScan(IdlStruct, typename="LaserScan"):
+    header: Header
+    angle_min: float32
+    angle_max: float32
+    angle_increment: float32
+    time_increment: float32
+    scan_time: float32
+    range_min: float32
+    range_max: float32
+    ranges: List[float32]
+    intensities: List[float32]
 
 
 def message_callback(sample):
     print("MESSAGE RECEIVED : {}".format(sample.payload))
 
+
+STATE_LOST = 0
+STATE_ALIGN_RIGHT = 1
+STATE_ALIGN_LEFT = 2
+STATE_FORWARD = 3
+STATE_BACKWARD = 4
+STATE_FINISH = 5
 
 class MainView:
     def __init__(self, width, height, session):
@@ -33,20 +72,22 @@ class MainView:
         self.message_subscriber = self.session.declare_subscriber("turtle/debug_message", message_callback)
 
         self.interface = Interface()
-        self.interface.add_gui(Used(pygame.K_UP, "↑", (200, 500), self.turtle_up))
-        self.interface.add_gui(Used(pygame.K_DOWN, "↓", (200, 550), self.turtle_down))
-        self.interface.add_gui(Used(pygame.K_LEFT, "←", (175, 525), self.turtle_left))
-        self.interface.add_gui(Used(pygame.K_RIGHT, "→", (225, 525), self.turtle_right))
-        
+        self.interface.add_gui(Used(pygame.K_UP, "↑", (200, 500), self.turtle_up, self.turtle_standby_up))
+        self.interface.add_gui(Used(pygame.K_DOWN, "↓", (200, 550), self.turtle_down, self.turtle_standby_down))
+        self.interface.add_gui(Used(pygame.K_LEFT, "←", (175, 525), self.turtle_left, self.turtle_standby_left))
+        self.interface.add_gui(Used(pygame.K_RIGHT, "→", (225, 525), self.turtle_right, self.turtle_standby_right))
+
         self.last_distance = 0
         self.last_points = []
+        self.state = STATE_FINISH
+        self.last_state = -1
         
         pygame.font.init()
         self.font = pygame.font.SysFont('Comic Sans MS', 30)
 
     def quit(self):
         self.camera_image_subscriber.undeclare()
-        self.lidar_image_subscriber.undecalre()
+        self.lidar_image_subscriber.undeclare()
         self.cmd_vel_publisher.undeclare()
         self.message_publisher.undeclare()
         self.message_subscriber.undeclare()
@@ -55,20 +96,21 @@ class MainView:
         image = np.frombuffer(bytes(sample.value.payload), dtype=np.uint8)
         image = cv2.imdecode(image, 1)
         image = cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
-                
+
         ret_qr, decoded_info, points, _ = self.qcd.detectAndDecodeMulti(image)
+        quad = points[0] if points is not None else None
         if points is not None:
-            image = cv2.polylines(image, points.astype(int), True, (255,0,0), 3)
-            
-            quad = points[0]
+            image = cv2.polylines(image, points.astype(int), True, (255, 0, 0), 3)
+
             distance = self.calcDistance(quad)
-            
+
             self.last_distance = distance
             
-            self.last_points = points[0]
+        self.update_state(image.shape, quad)
+            
         
         self.camera_image = pygame.surfarray.make_surface(image)
-        
+
     def calcDistance(self, points):
         knownWidth = 100
         knownDistance = 40
@@ -78,11 +120,12 @@ class MainView:
         width = np.mean([distanceBtw(points[i],points[i+1]) for i in range(3)])
         
         distance = (knownDistance * knownWidth) / width
-        
+
         return distance
 
     def lidar_image_callback(self, sample):
         print('[DEBUG] Received frame: {}'.format(sample.key_expr))
+
         scan = LaserScan.deserialize(sample.payload)
         angles = list(
             map(lambda x: x * 1j + cmath.pi / 2j, np.arange(scan.angle_min, scan.angle_max, scan.angle_increment)))
@@ -93,6 +136,7 @@ class MainView:
         X = [i.real for i in complexes]
         Y = [i.imag for i in complexes]
         XY = [[i.real, i.imag] for i in complexes]
+
         self.patch.set_xy(XY)
         self.line.set_data(X, Y)
 
@@ -107,16 +151,16 @@ class MainView:
         self.lidar_image = pygame.image.fromstring(data, size, mode)
 
     def turtle_up(self):
-        self.cmd_vel_publisher.put(("Forward", 20.0))
+        self.cmd_vel_publisher.put(("Forward", 10.0))
 
     def turtle_down(self):
-        self.cmd_vel_publisher.put(("Forward", -20.0))
+        self.cmd_vel_publisher.put(("Forward", -10.0))
 
     def turtle_left(self):
-        self.cmd_vel_publisher.put(("Rotate", 150.0))
+        self.cmd_vel_publisher.put(("Rotate", 35.0))
 
     def turtle_right(self):
-        self.cmd_vel_publisher.put(("Rotate", -150.0))
+        self.cmd_vel_publisher.put(("Rotate", -35.0))
 
     def turtle_standby_up(self):
         self.cmd_vel_publisher.put(("Forward", 0.0))
@@ -142,21 +186,51 @@ class MainView:
     def update(self):
         self.interface.update()
         
-    def control_turtle(self):
-        ALIGNMENT_TOLERANCE = 50 
+        if self.state != self.last_state:
+            
+            self.turtle_standby_down()
+            self.turtle_standby_left()
+            
+            match self.state:
+                case 0: 
+                    self.turtle_right()
+                case 1: 
+                    self.turtle_left()
+                case 2: 
+                    self.turtle_right()
+                case 3: 
+                    self.turtle_up()
+                case 4: 
+                    self.turtle_down()
+                case _:
+                    pass
+                
+            self.last_state = self.state
         
-        position = np.mean(self.last_points)
+    def update_state(self, imageShape, quad):
+        ALIGNMENT_TOLERANCE = 75 
+        POSITION_TOLERANCE = 5
         
-        width, height = self.surface_configuration
+        width, height = imageShape[:2]
         
-        if self.last_points is None:
-            self.turtle_right()
+        if quad is None:
+            self.state = STATE_LOST
+            return
+        
+        position = np.mean(quad[:,1])
+        print(position)
+        distance = self.calcDistance(quad)
+        
         if position > width / 2 + ALIGNMENT_TOLERANCE:
-            self.turtle_right()
+            self.state = STATE_ALIGN_RIGHT
         elif position < width / 2 - ALIGNMENT_TOLERANCE:
-            self.turtle_left()
+            self.state = STATE_ALIGN_LEFT
+        elif distance > 30 + POSITION_TOLERANCE :
+            self.state = STATE_FORWARD
+        elif distance < 30 - POSITION_TOLERANCE :
+            self.state = STATE_BACKWARD
         else:
-            self.turtle_up()
+            self.state = STATE_FINISH
         
     
 
@@ -173,10 +247,9 @@ class MainView:
                                                     self.lidar_image.get_height() + 10))
             surface.blit(self.lidar_image, (805, 25))
             
-        text_surface = self.font.render(f'Distance: {self.last_distance:.2f}cm', False, (0, 0, 0))
+        text_surface = self.font.render(f'Distance: {self.last_distance:.2f}cm\n State:{self.state}', False, (0, 0, 0))
         
         surface.blit(text_surface, 50,400)
         
-        self.control_turtle()
 
         self.interface.render(surface)
